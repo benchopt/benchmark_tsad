eb892258c922fc00d6b851567a0f6130ab3703e2
Jad-yehya <jadyehya@hotmail.com>
2024-06-28 16:48:20 +0200

    Updated test_config, skipping DIF if no gpu

--- test_config.py
+++ test_config.py
@@ -2,6 +2,8 @@ import sys  # noqa: F401
 
 import pytest  # noqa: F401
 
+from benchopt.utils.sys_info import get_cuda_version
+
 
 def check_test_solver_install(solver_class):
     """Hook called in `test_solver_install`.
@@ -10,4 +12,6 @@ def check_test_solver_install(solver_class):
     particular architecture, call pytest.xfail when
     detecting the situation.
     """
-    pass
+    if solver_class.name.lower() == "dif":
+        if get_cuda_version() is None:
+            pytest.xfail("Deep IsolationForest needs a working GPU hardware.")
