3efbc35243739f222555ccacdcd40a144b578f64
Jad-yehya <jadyehya@hotmail.com>
2024-07-11 11:23:59 +0200

    New Precision metric

--- benchmark_utils/__init__.py
+++ benchmark_utils/__init__.py
@@ -9,43 +9,11 @@ with safe_import_context() as import_ctx:
     import numpy as np
 
 
-# Implementing a precision metric, if the anomaly is detected in a window,
-# we consider it as detected
-def soft_precision(y_true, y_pred, detection_range=3):
-    """
-    If an anomaly is detected detection_range windows before or after
-    the true anomaly, we consider it as detected
-    e.g. detection_range=3, if anomaly detected at timestamp 5,
-    we consider y_pred correct if there is an anomaly at timestamp 2,..., 8
-    """
-    y_true_windows = np.lib.stride_tricks.sliding_window_view(
-        y_true, window_shape=detection_range*2+1, axis=0)
-    idxs = y_true_windows[:, detection_range]
-    # idx : iterate over y_pred
-    # i : iterate over y_true_windows
-    # First element of y_true_windows: first detection_range elements of y_pred
-
-    precision = 0
-    for i in range(detection_range):
-        if y_pred[i] == 1 and np.any(y_true[:detection_range] == 1):
-            precision += 1
-
-    for i, idx in enumerate(idxs):
-        if y_pred[i] == 1 and np.any(y_true_windows[i] == 1):
-            precision += 1
-
-    for i in range(len(y_pred)-detection_range, len(y_pred)):
-        if y_pred[i] == 1 and np.any(y_true[detection_range:] == 1):
-            precision += 1
-
-    return precision / np.sum(y_pred)
-
-
-def soft_precision2(y_true: np.ndarray,
-                    y_pred: np.ndarray,
-                    detection_range=3,
-                    return_counts=False,
-                    ):
+def soft_precision(y_true: np.ndarray,
+                   y_pred: np.ndarray,
+                   detection_range=3,
+                   return_counts=False,
+                   ):
     """Ratio of correctly detected anomalies to the number of total anomalies
     If an anomaly is detected detection_range windows before or after
     the true anomaly, we consider it as detected
@@ -96,7 +64,7 @@ def soft_precision2(y_true: np.ndarray,
             ma += 1
 
         if y_true[i] == 1 and (
-                y_true[left:right] == 1).any():
+                y_pred[left:right] == 1).any():
             da += 1
 
     # Removing exact matches from detected anomalies because they are
