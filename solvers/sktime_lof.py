df70b2072467efc35065351a96d4bc1c385a64f5
Jad-yehya <jadyehya@hotmail.com>
2024-09-26 13:59:13 +0200

    improved solver

--- solvers/sktime_lof.py
+++ solvers/sktime_lof.py
@@ -3,13 +3,15 @@ from benchopt import safe_import_context
 
 with safe_import_context() as import_ctx:
     from sktime.annotation.lof import SubLOF
+    import pandas as pd
+    import numpy as np
 
 
 class Solver(BaseSolver):
     name = "SubLOF"
 
     install_cmd = "conda"
-    requirements = ["sktime"]
+    requirements = ["sktime", "pandas"]
 
     parameters = {
         "n_neighbors": [5, 10, 20, 25, 40],
@@ -21,20 +23,21 @@ class Solver(BaseSolver):
     sampling_strategy = "run_once"
 
     def set_objective(self, X_train, y_test, X_test):
-        self.X_train = X_train
-        self.X_test, self.y_test = X_test, y_test
+        self.X_train = pd.DataFrame(X_train)
+        self.X_test, self.y_test = pd.DataFrame(X_test), y_test
         self.clf = SubLOF(
             n_neighbors=self.n_neighbors,
             window_size=self.window_size,
             leaf_size=self.leaf_size,
             contamination=self.contamination,
             n_jobs=-1,
+            novelty=True,
         )
 
     def run(self, _):
         self.clf.fit(self.X_train)
         self.raw_y_hat = self.clf.predict(self.X_test)
-        self.raw_anomaly_score = self.clf.predict_score(self.X_test)
+        # self.raw_anomaly_score = self.clf.predict_score(self.X_test)
 
     def skip(self, X_train, y_test, X_test):
         if self.n_neighbors > self.window_size:
@@ -48,5 +51,6 @@ class Solver(BaseSolver):
         return False, None
 
     def get_result(self):
-        self.y_hat = self.raw_y_hat
+        self.y_hat = np.zeros(self.X_test.shape[0])
+        self.y_hat[self.raw_y_hat] = 1
         return dict(y_hat=self.y_hat)
